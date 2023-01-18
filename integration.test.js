126436d6923fabddce545ac02dbef5b946662d6e
lesimoes <lesimoes0@gmail.com>
2023-01-18 19:08:09 -0300

    changed github actions

--- integration.test.js
+++
@@ -1,9 +0,0 @@
-import assert from 'node:assert';
-import supertest from "supertest";
-import { app } from "./index.js";
-
-
-const server = supertest(app);
-const response = await server.get("/users");
-console.log(response.status)
-assert(response.status === 200, 'Deve retornar status 200')
